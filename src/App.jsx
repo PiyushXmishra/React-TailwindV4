7d1fa923544f80b0c526e01a64fe3f2bdec04f89
PiyushXmishra <piyushbhp83@gmail.com>
2025-01-29 16:08:16 +0530

    Basic Template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-purple-300">
+      TailWind Rocks!
+    </h1>
+  );
+}
+
+export default App;
